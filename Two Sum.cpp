1e2459ca985c776ff3e44c69c3fd412bf9a57183
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-07 16:41:46 -0700

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -7,7 +7,7 @@ public:
          int b=0;
 
         
-        for(int i=0;i<nums.size()-1;i++)
+        for (int i=0;i<nums.size()-1;i++)
         { 
                 
                 for(int j=i+1;j<nums.size();j++)
