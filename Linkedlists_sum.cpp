17bfb3b114079482e4e0c3b4015b7ccee0a9b829
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-17 11:12:41 -0700

    Update Linkedlists_sum.cpp

--- Linkedlists_sum.cpp
+++ Linkedlists_sum.cpp
@@ -8,48 +8,51 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 
-      int c=0,ones=0;
-      struct ListNode *l3, *t, *last;
-      struct ListNode *p;
-      
-      while(p!=NULL)
+      int c=0,ones=0, arr [3];
+      struct ListNode *l3, *last;
+      struct ListNode *p,*l;
+     
+      l= new ListNode; 
+      l->val=l1->val;
+      l->next=NULL;   
+      last=l;
+
+      while(c!=3)
       {
-        c++;
-        p=p->next;
-      }
-      for (int i=0;i<c;i++)
-      {
-          int temp1= 2;
-          int temp2= 3;
-          int temp=temp1+temp2;
-          if (temp>=0 && temp<=9)
+          
+          l3= new ListNode;         
+          int temp=l1->val+l2->val;
+          if(temp==NULL)
           {
-              //l3->val=temp+ones;
-              //t = new ListNode;
-              int d =1+2;
-              //t->next=NULL;
-              //last->next=t;
-              //last=t;
+              break;
+          }
+          
+          if (temp>=0 && temp<=9)
+          {            
+              l3->val=temp+ones;
           }
           else
           {
-              //int digit = temp%10;
-              //int number = temp/10;
-              //l3[i]=digit+ones;
-              int y =1+2;
-              //ones=number;
-               
+              int digit = temp%10;
+              int number = temp/10;
+              l3->val=digit+ones;
+              ones=number;             
           }
-
-          //t->next=NULL;
-          //last->next=t;
-          //last=t;
-      
-      }
+          l1=l1->next;
+          l2=l2->next;
+          
+          l3->next=NULL;
+          last->next=l3;
+          last=l3;                 
+          c++;
+          cout<<l3->val;
+      }     
       return l3;
     }     
 };
