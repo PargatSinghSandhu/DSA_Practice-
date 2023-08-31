ed83a8755d9f37d21882c7b38cae34c364154c25
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-08-30 19:10:43 -0600

    Fibonacci Number_DP.cpp

--- 509. Fibonacci Number_DP.cpp
+++ 509. Fibonacci Number_DP.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int fib(int n) {
-        
-        vector<int> dp(n+1,-1);
-        return fi(n, dp);
-        
 
-
+       vector<int>dp(n+1, -1 );  //0
+       return fi(n, dp);
     }
-    int fi(int n, vector<int> &dp)
+
+
+    int fi(int n, vector<int>&dp)
     {
         if(n<=1)
         {
             return n;
         }
 
-        if(dp[n]!=-1)
+
+        if(dp[n]!=-1)       //2
         {
             return dp[n];
         }
-
-        return dp[n] = fi(n-1, dp)+fi(n-2,dp);
-
-    }        
+        return dp[n]=fi(n-1,dp)+fi(n-2,dp); //1
+    }
 };
