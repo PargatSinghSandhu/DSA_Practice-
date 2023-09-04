fe57e90513cf1268b57c38476ebc640ebb03c6bf
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-09-04 15:50:05 -0600

    Create 46. Permutations_Swap.cpp

---
+++ 46. Permutations_Swap.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+
+        vector<vector<int>> ans;
+        recurPerm(0, nums, ans);
+        return ans;
+
+    }
+
+    void recurPerm(int index, vector<int>&nums , vector<vector<int>> & ans)
+    {
+        if(index==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+
+        for(int i=index;i<nums.size();i++)
+        {
+            swap(nums[index], nums[i]);
+            recurPerm(index+1, nums, ans);
+            swap(nums[index], nums[i]);
+        }
+    }
+};
