301d8923b9ce5889b0ebcf52022b2922ae9fe573
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-09-18 13:20:03 -0600

    Update 189. Rotate Array_Optimal.cpp

--- 189. Rotate Array_Optimal.cpp
+++ 189. Rotate Array_Optimal.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
 
-        k = k%nums.size();
-        reverse(nums.begin(), nums.begin()+(nums.size()-k));
+         k = k%nums.size();
+         reverse(nums.begin(), nums.begin()+(nums.size()-k));   // nums.begin = 0, nums.begin()+(nums.size()-k) = 4, it will run 0-3
+         reverse(nums.begin()+(nums.size()-k), nums.end());
+         reverse(nums.begin(), nums.end());
         
-        reverse(nums.begin()+(nums.size()-k) , nums.end());
-        
-        reverse(nums.begin(), nums.end());
-
-    }};
+    }
+};
