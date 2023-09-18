46c30143105d6c7c9d1d2b74b2930211fce27730
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-09-18 12:39:14 -0600

    Update 189. Rotate Array_Brute.cpp

--- 189. Rotate Array_Brute.cpp
+++ 189. Rotate Array_Brute.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         
-        int d = k%nums.size(), n = nums.size();
+        int d=k%nums.size();
         vector<int>temp;
+        int n = nums.size();
 
         for(int i=0;i<d;i++)
         {
             temp.push_back(nums[(n-d)+i]);
         }
-
-        for (int i = n - 1; i >= d; i--) 
+        for(int i = n-1;i>=d;i--)
         {
-            nums[i] = nums[i - d];
+            nums[i]=nums[i-d];
         }
         
-        int p=0;
-
-        for(int r = 0;r<d;r++)
+        for(int i=0;i<d;i++)
         {
-            nums[r]=temp[p];
-            p++;
+            nums[i]=temp[i];
         }
-    }};
+    }
+};
