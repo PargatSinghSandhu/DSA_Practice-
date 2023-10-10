6015ed90c6a51c5abe61a8e3e3ef00f81400ea5b
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-10-10 11:19:47 -0600

    Update 31. Next Permutation.cpp

--- 31. Next Permutation.cpp
+++ 31. Next Permutation.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int index = -1;
+        
         int n = nums.size();
+        int index = -1;
 
-        for(int i = n-2; i>=0; i--)
+        // we have to find the dip, where the value is decreasding.
+
+        for(int i = n-2; i >=0; i--)
         {
             if(nums[i]<nums[i+1])
             {
-                index=i;
+                index = i;
                 break;
-            } 
-              
+            }
         }
-        if(index == -1)
+
+        if(index ==-1) // there is no dip, so the next greatest order will be reverse of the order.
         {
             reverse(nums.begin(), nums.end());
-            return;         
-        }
-        for(int i=n-1; i>index;i--)
+            return;
+
+        } 
+        // we have found the dip, now we to replace that dip with the smallest number picking from the rest elemetns.
+
+        for(int i = n-1;i>index;i--)
         {
-            if(nums[i]>nums[index])
+            if(nums[index]<nums[i])
             {
                 swap(nums[i], nums[index]);
                 break;
-            }    
+            }
         }
+
         reverse(nums.begin()+index+1, nums.end());
         return;
-        
+
+
+
     }
 };
-
-
