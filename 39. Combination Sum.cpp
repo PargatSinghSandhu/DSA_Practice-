fb0e0e653168dc1f21768c29f70f7807157d63ec
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-10-11 20:30:25 -0600

    Update 39. Combination Sum.cpp

--- 39. Combination Sum.cpp
+++ 39. Combination Sum.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-
-        int n = candidates.size();
-        vector<int> sub;
-        vector<vector<int>>ans;
-        comb(0, target, candidates, sub, ans, n);
+        
+        vector<vector<int>> ans;
+        vector<int>ds;
+        comb(0, target, ans, candidates,ds);
         return ans;
- 
-        
-      
     }
 
-
-    void comb(int index, int target, vector<int>& candidates, vector<int> & sub,  vector<vector<int>>&ans, int n)
+    //will pick one element and if that doesn't satisy the condition will pick another, increament the index
+    void comb(int index, int target, vector<vector<int>> & ans, vector<int>& candidates, vector<int>&ds )
     {
-        if(target==0)
-        {
-            ans.push_back(sub);
-            return;
-        }
-        else if(index==n)
-        {
-            return;
-        }
         
-        if(candidates[index]<=target)
+        if(target == 0) //if target reaches to 0, then we can add the vector, because size does'nt matter so there can be any size of vector can be added 
         {
-            sub.push_back(candidates[index]);
-            comb(index, target-candidates[index], candidates, sub, ans, n);
-            sub.pop_back();
-            
+            ans.push_back(ds);
+            return;
         }
-        comb(index+1, target, candidates, sub, ans,n);
+        else if(index==candidates.size()) //if index reaches to size of the nums vector but does'nt shows target ==0 
+        {
+            return;
+        }
+
+
+        if(candidates[index]<=target)  // check every element if its less than the target
+        {
+            ds.push_back(candidates[index]);    // if less than the target then the element adds in the vector
+            comb(index, target-candidates[index], ans, candidates,ds); //call the recursive function
+            ds.pop_back();
+
+        }
+        comb(index+1, target, ans, candidates,ds);  // if the upper condition isn't passed then we move to another index and check again the another element.
+
     }
-
-
 };
