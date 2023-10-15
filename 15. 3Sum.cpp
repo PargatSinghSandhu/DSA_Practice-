85a7bcb7d7ca392218ad132a355a03101d583571
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-10-15 16:39:57 -0600

    Update 15. 3Sum.cpp

--- 15. 3Sum.cpp
+++ 15. 3Sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
     vector<vector<int>> ans;
     int n = nums.size();
-    sort(nums.begin(), nums.end());
+     sort(nums.begin(), nums.end());
 
      for(int i=0; i<n;i++)
      {
