966c32b336653bea37558a583d96177bda7f0178
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-10-21 22:48:12 -0600

    Update Next Greater Element.cpp

--- Next Greater Element.cpp
+++ Next Greater Element.cpp
@@ -5,30 +5,30 @@ public:
         int n=nums2.size();
         stack<int> st;
         vector<int> ans;
-        map<int, int> nge;
+        map<int, int> nge;        // using the map to store the value against the key and then use it later to use for getting the element from the array 1.
 
-        for(int i = n-1;i>=0;i--)
+        for(int i = n-1;i>=0;i--)        // running the loop from the back, as this is non-circular array, so starting from the last element.
         {
-            while(!st.empty() && nums2[i]>=st.top())
+            while(!st.empty() && nums2[i]>=st.top())        //it will check if the stack is not empty and the current element of the array is greater than the top element of the stack then, it will pop the element until the condition gets false
             {
-                st.pop();
+                st.pop(); //2 cases, 1. every element is less than the current element, then stack will gets empty. 2. if top element is less than the current element, and next one is greater then it will stop and there will be some element in the stack 
             }
 
-            if(st.empty())
+            if(st.empty()) // if the stack is empty, then this 
             {
                 nge[nums2[i]] = -1;
             }
-            else 
+            else     //if the stack is not empty, then the top element will be used
             {
                 nge[nums2[i]] = st.top();
 
             }
-            st.push(nums2[i]);
+            st.push(nums2[i]); // pushing the current element in the stack.
         }
 
-        for(int i = 0;i<nums1.size();i++)
-        {
-            ans.push_back(nge[nums1[i]]);
+        for(int i = 0;i<nums1.size();i++) // for loop for the nums1.
+        { 
+            ans.push_back(nge[nums1[i]]); // we have the designated values agasint the keys, which we will use to get the value for the nums1. 
         }
         return ans;
     }
