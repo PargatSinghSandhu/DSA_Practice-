075359d0d2f181151df8dcd1ee01c6d6fc3764c7
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-10-23 02:52:48 -0600

    Update 1752. Check if Array Is Sorted and Rotated_Brute.cpp

--- 1752. Check if Array Is Sorted and Rotated_Brute.cpp
+++ 1752. Check if Array Is Sorted and Rotated_Brute.cpp
@@ -1,41 +1,14 @@
-class Solution 
-{
+class Solution {
 public:
-    bool check(vector<int>& nums)
-    {
-       vector<int> numsbp = nums;
-       sort(numsbp.begin(), numsbp.end());
-
-       if(nums==numsbp)
-       {
-           return true;
-       }
-
-       for(int i =1; i< nums.size();i++)
-       {
-           int p = 1;
-           vector<int> numsb = nums;
-           sort(numsb.begin(), numsb.end());
-           while (p <= i) 
-           {
-                int last = numsb[0];
-                for (int i = 0; i < numsb.size() - 1; i++) 
-                {
-                    numsb[i] = numsb[i + 1];
-                }
-                numsb[numsb.size() - 1] = last;
-                p++;
+    bool check(vector<int>& nums) {
+        int count =0;
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(nums[i]>nums[(i+1)%nums.size()]) // check increasing order, this condition is for decreasingorder 
+            {
+                count++;
             }
-
-           if(numsb==nums)
-           {
-               return true;
-           }
-
-       }
-       return false;
-        
+        }
+        return count<=1;
     }
 };
-
-
