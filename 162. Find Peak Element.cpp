dcf71a5e91105ca6bec178888c4db870939462e3
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-10-26 15:43:45 -0600

    Update 162. Find Peak Element.cpp

--- 162. Find Peak Element.cpp
+++ 162. Find Peak Element.cpp
@@ -1,37 +1,32 @@
- class Solution {
+class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
+    int findPeakElement(vector<int>& nums) {
         
-        int n = arr.size();
-        
-        if(n<3||arr[0]>arr[1])
-        {
-            return 0;
-        }       
-        if(arr[n-1]>arr[n-2]) return n-1;
-        
-        int low = 1, high = n-2, mid, ans;
+        int n=  nums.size();
+        if(n==1) return 0; // if size is 1 then return that single element/index
+        if(nums[0]>nums[1]) return 0;  //if the first element is greater than the second element then return first inde, no need to go further
+        if(nums[n-1]>nums[n-2]) return n-1; // if the last element is greater than the seond last return the last element.
+
+        int low = 1, high = nums.size()-2, mid;
 
         while(low<=high)
         {
             mid = (low+high)/2;
 
-            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1])
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
             {
-                ans= mid;
+                return mid;
             }
-
-            if (arr[mid]<arr[mid+1] && arr[mid]>arr[mid-1])
+            if( nums[mid-1]< nums[mid] && nums[mid] < nums[mid]<nums[mid+1])
             {
+                
                 low = mid+1;
             }
             else
             {
                 high = mid-1;
             }
-
-            
         }
-        return ans;
+        return -1;
     }
 };
